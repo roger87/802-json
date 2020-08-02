a2f2b3fc1711503659b7a3ea33eeb34b425f1499
cso1999888 <cso1999888@gmail.com>
2020-08-02 12:33:32 +0800

    修改

--- restaurant.js
+++ restaurant.js
@@ -1,9 +1,18 @@
 new Vue({
-    el:"#resaurant",
+    el: "#resaurant",
     data: {
-        datas: null
+        datas: null // 資料: 空值
     },
+    // 顯示所有元素後執行
     mounted() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://datacenter.taichung.gov.tw/swagger/OpenData/72b2e32b-74e4-4000-b920-7457c54565be");
+        xhr.send();
 
+        xhr.onload = () => {
+            var json = JSON.parse(xhr.responseText);
+            this.datas = json;
+            console.log(json);
+        }
     },
 });
